fdc7361910bbcd880efcb54e90dcf8bff10b4bbe
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-04-15 20:34:18 -0300

    UUID added to message handler

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -1,17 +1,18 @@
 from common import message_protocol
 
+import uuid
 
 class MessageHandler:
 
     def __init__(self):
-        pass
+        self.messages_id = uuid.uuid4()
     
     def serialize_data_message(self, message):
         [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
+        return message_protocol.internal.serialize([self.messages_id, fruit, amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+        return message_protocol.internal.serialize([self.messages_id])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
